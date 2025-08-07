a3446dd216c990811ecbe97e351a5ce090e047cc
Deekshitha1203 <deekshithasuddamandu@gmail.com>
2025-08-07 22:02:16 +0530

    Update app.java

--- app.java
+++ app.java
@@ -10,3 +10,14 @@ class Main {
     System.out.println(first + " + " + second + " = "  + sum);
   }
 }
+
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.fo
